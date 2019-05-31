a25e4e65274799c1be087bda7701a1b846edd9aa
Your Name <you@example.com>
2019-05-30 23:00:46 -0300

    identado

--- entrega2/client.c
+++ entrega2/client.c
@@ -4,16 +4,16 @@
 #include <stdlib.h>
 #include <netinet/in.h>
 #include <string.h>
-int main(int argc , char *argv[])
+int main(int argc, char *argv[])
 {
 	//Criar um for em volta da conexão para criar varios clientes
 	//receber a string do server e guardar num arquivo
 	int sock;
 	struct sockaddr_in server;
-	char message[1024] , server_reply[1024];
+	char message[1024], server_reply[1024];
 	//Tem que aumentar o tamanho do char de reply
 	// Criação do socket
-	sock = socket(AF_INET , SOCK_STREAM , 0);
+	sock = socket(AF_INET, SOCK_STREAM, 0);
 	if (sock == -1)
 	{
 		printf("Não foi possível criar o socket.");
@@ -21,12 +21,12 @@ int main(int argc , char *argv[])
 	puts("Socket criado");
 
 	// server.sin_addr.s_addr = inet_addr("127.0.0.1");
-    server.sin_addr.s_addr = INADDR_ANY;
+	server.sin_addr.s_addr = INADDR_ANY;
 	server.sin_family = AF_INET;
-	server.sin_port = htons( 8080 );
+	server.sin_port = htons(8080);
 
 	//Connect to remote server
-	if (connect(sock , (struct sockaddr *)&server , sizeof(server)) < 0)
+	if (connect(sock, (struct sockaddr *)&server, sizeof(server)) < 0)
 	{
 		perror("Conexão falhou. Erro.");
 		return 1;
@@ -34,33 +34,34 @@ int main(int argc , char *argv[])
 
 	puts("Conectado\n");
 
-    char exit_message[1024] = "sair";
+	char exit_message[1024] = "sair";
 	//keep communicating with server
 	// while(1)
 	// {
-		// printf("Digite uma mensagem: ");
-		// scanf("%s" , message);
-		sprintf(message, "GET /");
-		if(!strcmp(&message[1024], "sair")){
-			close(sock);
-		}
+	// printf("Digite uma mensagem: ");
+	// scanf("%s" , message);
+	sprintf(message, "GET /");
+	if (!strcmp(&message[1024], "sair"))
+	{
+		close(sock);
+	}
 
-		//Send some data
-		if( send(sock , message , strlen(message) , 0) < 0)
-		{
-			puts("Falha no envio da mensagem.");
-			return 1;
-		}
+	//Send some data
+	if (send(sock, message, strlen(message), 0) < 0)
+	{
+		puts("Falha no envio da mensagem.");
+		return 1;
+	}
 
-		//Receive a reply from the server
-		if( recv(sock , server_reply , 1024 , 0) < 0)
-		{
-			puts("Falha ao receber resposta do servidor.");
-			// break;
-		}
+	//Receive a reply from the server
+	if (recv(sock, server_reply, 1024, 0) < 0)
+	{
+		puts("Falha ao receber resposta do servidor.");
+		// break;
+	}
 
-		puts("Resposta do servidor: ");
-		puts(server_reply);
+	puts("Resposta do servidor: ");
+	puts(server_reply);
 	// }
 
 	// puts("Para sair envie 'sair'");
--- entrega2/main.cpp
+++ entrega2/main.cpp
@@ -6,8 +6,8 @@
 #include <string.h>
 #include <pthread.h>
 #include <ctime>
-#include <iostream>       // std::cin, std::cout
-#include <queue>          // std::queue
+#include <iostream> // std::cin, std::cout
+#include <queue>    // std::queue
 
 using namespace std;
 
@@ -18,13 +18,14 @@ using namespace std;
 #define NUMTHREADS 3
 
 // dados da thread
-typedef struct{
+typedef struct
+{
     pthread_t pthread;
     pthread_mutex_t mutex_visits;
     pthread_mutex_t mutex_cond;
-    pthread_cond_t  block_thread;
-    pthread_cond_t  return_thread;
-}THREAD_DATA;
+    pthread_cond_t block_thread;
+    pthread_cond_t return_thread;
+} THREAD_DATA;
 
 // *********** VARIÁVEIS GLOBAIS ********* //
 // do número de clientes conectados ao servidor.
@@ -42,22 +43,25 @@ std::string timestamp_now = std::to_string((int)time(NULL));
 std::string url = timestamp_now + ".txt";
 char filename[20];
 
-
-void log_to_file(char *file_name, char *info) {
+void log_to_file(char *file_name, char *info)
+{
     time_t rawtime;
-    struct tm * timeinfo;
+    struct tm *timeinfo;
     char timenow[80];
 
-    time (&rawtime);
+    time(&rawtime);
     timeinfo = localtime(&rawtime);
 
-    strftime(timenow,sizeof(timenow),"%Y-%m-%d %H:%M:%S",timeinfo);
+    strftime(timenow, sizeof(timenow), "%Y-%m-%d %H:%M:%S", timeinfo);
 
     FILE *arq;
     arq = fopen(file_name, "a");
-    if(arq == NULL) {
+    if (arq == NULL)
+    {
         printf("Erro. Não foi possível criar o arquivo de log.");
-    } else {
+    }
+    else
+    {
         fprintf(arq, "%s - ", timenow);
         fprintf(arq, "%s\r\n", info);
         // puts("\r\n", arq);
@@ -69,9 +73,11 @@ void log_to_file(char *file_name, char *info) {
 }
 
 // ********* Método que trata as requisições ao servidor ********* //
-void *process_request(void * t_data) {
+void *process_request(void *t_data)
+{
     THREAD_DATA *td = (THREAD_DATA *)t_data;
-    while (!pthread_cond_wait(&td->block_thread, &td->mutex_cond)) {
+    while (!pthread_cond_wait(&td->block_thread, &td->mutex_cond))
+    {
 
         int sock;
         sock = fila_socket.front();
@@ -84,57 +90,54 @@ void *process_request(void * t_data) {
 
         pthread_mutex_lock(&td->mutex_visits);
         visitantes++;
-        sprintf(response,"Você é o #%dº visitante!!! Muito obrigada :) Para sair basta enviar \"sair\".\n ", visitantes);
+        sprintf(response, "Você é o #%dº visitante!!! Muito obrigada :) Para sair basta enviar \"sair\".\n ", visitantes);
         pthread_mutex_unlock(&td->mutex_visits);
         log_to_file(filename, "[OK] Thread desbloqueada.");
 
         read(sock, buffer, 1024);
         printf("%s\n", buffer);
         char get[1024] = "GET /";
-        if(!strcmp(buffer, get)){
+        if (!strcmp(buffer, get))
+        {
             send(sock, response, strlen(response), 0);
             log_to_file(filename, "[ERRO] Mensagem de boas-vindas enviada.");
             printf("Ḿensagem de oi enviada\n");
             printf("Cliente saiu");
 
             close(sock);
-            count--; 
-            }
-            else{
-                printf("404");
-                close(sock);
-                printf("Cliente saiu");
-                count--;
-
-            }
-            bzero(buffer, 1024);
-
-        
-
+            count--;
+        }
+        else
+        {
+            printf("404");
+            close(sock);
+            printf("Cliente saiu");
+            count--;
+        }
+        bzero(buffer, 1024);
     }
 }
 
-int main() {
+int main()
+{
     /********** Pega o tempo atual e cria um arquivo de log com o nome no formato yyyymmaahhmmss **********/
     time_t rawtime;
-    struct tm * timeinfo;
+    struct tm *timeinfo;
     char timenow[80];
     // char filename[20];
 
-    time (&rawtime);
+    time(&rawtime);
     timeinfo = localtime(&rawtime);
 
-    strftime(timenow,sizeof(timenow),"%Y%m%d%H%M%S",timeinfo);
+    strftime(timenow, sizeof(timenow), "%Y%m%d%H%M%S", timeinfo);
 
     strcpy(filename, timenow);
     strcat(filename, ".txt");
 
-
     log_to_file(filename, "[OK] Servidor iniciado.");
     printf("Arquivo de log criado com o nome: %s\n", filename);
 
-
-    FILE* arq;
+    FILE *arq;
     // // abrindo arquivo no modo de leitura
     // arq = fopen(url, "w");
     // if (arq == NULL) {
@@ -145,21 +148,21 @@ int main() {
     //     fclose(arq);
     // }
 
-
     // Print na tela
     // std::cout << timestamp_now << '\n' ;
 
-     // printf("Timestamp: %d\n",(int)time(NULL));
+    // printf("Timestamp: %d\n",(int)time(NULL));
 
     // Criação de vetor de threads, com tamanho NUMTHREADS definido pelo  #define acima
     THREAD_DATA thread[NUMTHREADS];
 
     // Inicialização do vetor de threads com valores nulos
-    for(int i = 0; i < NUMTHREADS; i++){
+    for (int i = 0; i < NUMTHREADS; i++)
+    {
         pthread_mutex_init(&thread[i].mutex_visits, NULL);
         pthread_mutex_init(&thread[i].mutex_cond, NULL);
-        pthread_cond_init (&thread[i].block_thread, NULL);
-        pthread_cond_init (&thread[i].return_thread, NULL);
+        pthread_cond_init(&thread[i].block_thread, NULL);
+        pthread_cond_init(&thread[i].return_thread, NULL);
         pthread_create(&thread[i].pthread, NULL, process_request, (void *)&thread[i]);
     }
 
@@ -179,21 +182,24 @@ int main() {
     }
 
     // Atando o socket à porta 8080 à força.
-    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt))) {
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, &opt, sizeof(opt)))
+    {
         perror("setsockopt");
         exit(EXIT_FAILURE);
     }
 
     address.sin_family = AF_INET;
     address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons( PORT );
+    address.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0) {
+    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0)
+    {
         log_to_file(filename, "[ERRO] Falha no bind.");
         perror("bind failed");
         exit(EXIT_FAILURE);
     }
-    if (listen(server_fd, 3) < 0) {
+    if (listen(server_fd, 3) < 0)
+    {
         log_to_file(filename, "[ERRO]. Falha ao escutar por conexões.");
         perror("listen");
         exit(EXIT_FAILURE);
@@ -202,14 +208,17 @@ int main() {
     log_to_file(filename, "[OK] Socket criado. Aguardando conexões...");
 
     // Rodar para sempre para que o servidor fique escutando à espera de novas conexões.
-    while(1) {
-        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t*)&addrlen))<0) {
+    while (1)
+    {
+        if ((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0)
+        {
             log_to_file(filename, "[ERRO] Falha ao aceitar conexão.");
             perror("accept");
             exit(EXIT_FAILURE);
         }
         fila_socket.push(new_socket);
-        if(count < NUMTHREADS){
+        if (count < NUMTHREADS)
+        {
             pthread_mutex_lock(&thread[count].mutex_cond);
             printf("%d", new_socket);
             log_to_file(filename, "[OK] Socket adicionado à fila.");
